3296f0b9b163a66cbf480d55239391383448a0a1
Rachel Uberti <rachel.uberti@gmail.com>
2023-12-05 14:23:22 +1100

    Feat: notes

--- src/utils/zendesk.js
+++ src/utils/zendesk.js
@@ -1,5 +1,7 @@
+//zendesk app framework
 export const zaf = ZAFClient.init()
 
+// async function - you are calling something and you dont when it will respond. Will not continue until await is fulfilled. 
 export async function getContext() {
   const response = await zaf.context()
   return response
