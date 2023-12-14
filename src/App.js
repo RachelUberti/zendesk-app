7378f4fed0dfa5994910c8bcbe91a605c490f110
Rachel Uberti <rachel.uberti@gmail.com>
2023-12-14 15:24:27 +1100

    Feat: Tabs and useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
-import Tabs from "./components/Tabs";
+import CustomerView from './pages/CustomerView'
+import SearchView from './pages/SearchView'
+import { calculateWindowHeight } from './utils/zendesk'
 
 const App = () => {
-  const tabData = [
-        { label: "Customer Details" },
-        { label: "Search" },
-  ];
+  const [tab, setTab] = useState("customer") 
+
+  // Only run this if tab state changes as its a dependency
+  // useEffect(() => {
+  //   console.log('ouch')
+  //   calculateWindowHeight()
+  // }, [tab])
+
+  // Only run this ONCE, on render, for as long as App is visible this will not run again
+  // useEffect(() => {
+  //   console.log('ouch')
+  //   calculateWindowHeight()
+  // }, [])
+  
+  useEffect(() => {
+    console.log('ouch')
+    calculateWindowHeight()
+  })
+  
+  const handleTabChange = (tab) => {
+    setTab(tab);
+  }
+
   return (
     <>
       <Header />
-      <Tabs tabs={tabData} />
+      <button onClick={() => handleTabChange("search")}>Search</button>
+      <button onClick={() => handleTabChange("customer")}>Customer</button>
+      {tab === "customer" && ( <CustomerView /> )}
+      {tab === "search" && ( <SearchView /> )}
     </>
   )
 }
